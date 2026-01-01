f5c3b7b594603cd633909815346019ddf4a374bd
syapa <a.khekhneva@gmail.com>
2026-01-01 12:10:48 +0700

    added new examples and test

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,2 +1,13 @@
-def test_first_try():
-    print("Hello World!")
+import pytest
+
+@pytest.mark.slow
+def test_heavy_calculation():
+    pass
+
+@pytest.mark.integration
+def test_integration_with_external_api():
+    pass
+
+@pytest.mark.smoke
+def test_quick_check():
+    pass
