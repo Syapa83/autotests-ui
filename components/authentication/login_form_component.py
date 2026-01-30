dc4e02bb5030311e16840b69ad3cb27f35e39901
Syapa83 <akhekhneva@gmail.com>
2026-01-30 13:18:27 +0300

    added

--- components/authentication/login_form_component.py
+++ components/authentication/login_form_component.py
@@ -1,12 +1,13 @@
 from playwright.sync_api import Page, expect
 from components.base_component import BaseComponent
+from elements.input import Input
 
 
 class LoginFormComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
-        self.email_input = page.get_by_test_id('login-form-email-input').locator('input')
-        self.password_input = page.get_by_test_id('login-form-password-input').locator('input')
+        self.email_input = Input(page, 'login-form-email-input', 'Email')
+        self.password_input = Input(page, 'login-form-password-input', 'Password')
 
     # Метод для заполнения формы авторизации
     def fill(self, email: str, password: str):
@@ -14,5 +15,5 @@ class LoginFormComponent(BaseComponent):
         self.password_input.fill(password)
 
     def check_visible(self, email: str, password: str):
-            expect(self.email_input).to_have_value(email)
-            expect(self.password_input).to_have_value(password)
+        self.email_input.check_have_value(email)
+        self.password_input.check_have_value(password)
